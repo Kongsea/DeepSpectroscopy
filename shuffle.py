d65e90a2e40d4b7ebbfe7bb0a6be41f015e2c6ca
Kongsea <kongsea@gmail.com>
2017-06-15 20:07:32 +0800

    Create shuffle.py
    Used to shuffle binary file and csv file.

---
+++ shuffle.py
@@ -0,0 +1,50 @@
+import os
+import csv
+import time
+import numpy as np
+from tqdm import tqdm
+
+from utils import readCSV
+
+
+def shuffle_bin(spec_name, spec_shuffle_name, shuffle_seed, spec_length, bit_length=4):
+  length = (1 + spec_length) * bit_length
+  with open(spec_name, 'rb') as f, open(spec_shuffle_name, 'wb') as fw:
+  buf = ''
+  for i, ss in enumerate(tqdm(shuffle_seed)):
+    f.seek(ss * length)
+    buf += f.read(length)
+    if (i+1)%300 == 0:
+      fw.write(buf)
+      buf = ''
+  else:
+    fw.write(buf)
+    
+    
+def shuffle_csv(csv_lines, writename, shuffle_seed):
+  with open(writename, 'wb') as fw:
+    cw = csv.writer(fw)
+    cw.writerow(csv_lines[0])
+    for i in shuffle_seed:
+      cw.writerow(csv_lines[i + 1])
+      
+      
+def main():
+  csvName = 'original.csv'
+  csv_lines = readCSV(csvName)
+  spec_num = len(csv_lines) - 1
+  shuffle_seed_name = 'shuffle_seed.npy'
+  shuffle_seed = np.random.permutation(spec_num)
+  with open(shuffle_seed_name, 'wb') as frs:
+    np.save(frs, shuffle_seed)
+  
+  shuffle_csv(csv_lines, 'shuffle.csv', shuffle_seed)
+  
+  spec_length = 57144
+  spec_name = 'original.bin'
+  spec_shuffle_name = 'shuffle.bin'
+  shuffle_bin(spec_name, spec_shuffle_name, shuffle_seed, spec_length=spec_length)
+  
+  
+if __name__ == '__main__':
+  main()
