6ad30dbddeb62f638c0e5e04836d13432b85a9fb
Kongsea <Kongsea@gmail.com>
2017-07-04 17:22:52 +0800

    Modify shuffle.

--- shuffle.py
+++ shuffle.py
@@ -8,13 +8,13 @@ from utils import readCSV
 
 
 def shuffle_bin(spec_name, spec_shuffle_name, shuffle_seed, spec_length, bit_length=4):
-  length = (1 + spec_length) * bit_length
+  length = (10 + spec_length) * bit_length
   with open(spec_name, 'rb') as f, open(spec_shuffle_name, 'wb') as fw:
   buf = ''
   for i, ss in enumerate(tqdm(shuffle_seed)):
     f.seek(ss * length)
     buf += f.read(length)
-    if (i+1)%300 == 0:
+    if (i + 1) % 300 == 0:
       fw.write(buf)
       buf = ''
   else:
@@ -30,19 +30,19 @@ def shuffle_csv(csv_lines, writename, shuffle_seed):
       
       
 def main():
-  csvName = 'original.csv'
+  csvName = 'data/original.csv'
   csv_lines = readCSV(csvName)
   spec_num = len(csv_lines) - 1
-  shuffle_seed_name = 'shuffle_seed.npy'
+  shuffle_seed_name = 'data/shuffle_seed.npy'
   shuffle_seed = np.random.permutation(spec_num)
   with open(shuffle_seed_name, 'wb') as frs:
     np.save(frs, shuffle_seed)
   
-  shuffle_csv(csv_lines, 'shuffle.csv', shuffle_seed)
+  shuffle_csv(csv_lines, 'data/shuffle.csv', shuffle_seed)
   
-  spec_length = 57144
-  spec_name = 'original.bin'
-  spec_shuffle_name = 'shuffle.bin'
+  spec_length = 1473
+  spec_name = 'data/original.bin'
+  spec_shuffle_name = 'data/shuffle.bin'
   shuffle_bin(spec_name, spec_shuffle_name, shuffle_seed, spec_length=spec_length)
   
   
